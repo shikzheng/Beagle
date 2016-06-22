a1686df24f0e8a3e40bbcf4434d406e2ef70e369
skz1996 <sz1389@nyu.edu>
2016-06-22 15:10:03 -0400

    Update Main.js

--- public_html/src/UI/Main.js
+++ public_html/src/UI/Main.js
@@ -4,6 +4,14 @@
 import React from 'react';
 import Container from '../Common/Container';
 import Section from './Section';
+import Section2 from './Section2';
+import Section3 from './Section3';
+import Section4 from './Section4';
+import Section5 from './Section5';
+import Select from './Select/Select';
+import Segment from './Segment/Segment';
+import Summarize from './Summarize/Summarize';
+import FieldList from './FieldList';
 
 
 class Main extends React.Component {
@@ -17,7 +25,8 @@ class Main extends React.Component {
         }
     }
     render() {
-        let SummarizeWidth = this.props.width - 350;
+        let SummarizeWidth = this.props.width;
+		let EmailHeight = this.props.height;
         let {dispatch, selectData, segmentData, sumarizeData, api} = this.props;
 
         let style ={
@@ -47,7 +56,28 @@ class Main extends React.Component {
         };
 
         return (
-          
+            <Container style={this.getStyle()}>
+			
+                <Section5 title="Filters" width="18%" left="0" color="#239CDC" >
+                </Section5>
+				
+
+				<Section3 title="Contacts" width="52%" color="#239CDC"  >
+						<Section2 width="35%" color="#55C94B">  					
+						</Section2>
+						<Section4 width="65%" color="#55C94B">                  
+						</Section4>
+				</Section3>
+				
+				
+				<Container  style={{display: "block", height:"50%", width:"30%"}}>
+					<Section title="Mentions" width="100%" color="#239CDC" >
+					</Section>
+					<Section title="Emails" width="100%"  color="#239CDC" >
+					</Section>
+				</Container>
+
+			</Container>
         );
     }
 }
