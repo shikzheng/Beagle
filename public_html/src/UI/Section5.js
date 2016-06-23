05f9cdfa80641d99bf9055b8ba11df6eec527d98
skz1996 <sz1389@nyu.edu>
2016-06-23 16:10:50 -0400

    Add files via upload

---
+++ public_html/src/UI/Section5.js
@@ -0,0 +1,84 @@
+/**
+ * Created by cristianfelix on 12/31/15.
+ */
+import React from 'react';
+import Container from '../Common/Container';
+var button = require('file!./AddButton.png');
+class Section extends React.Component {
+    getStyle() {
+        return {
+            position:"relative",
+            display: "flex",
+            flexDirection: "column",
+            height: "100%",
+            top: this.props.top,
+            width: this.props.width,
+            borderRight: "solid 1px #ccc"
+        }
+    }
+
+    render() {
+        let {type} = this.props;
+        console.log("Section.js",22, type)
+        let titleStyle = {
+            fontWeight: "bold",
+            color:"#00BFFF",
+            padding: 6,
+            fontSize: 30,
+            margin: 0
+        };
+		
+		let hrStyle = {
+			width: "95%",
+			color:this.props.color
+        };
+		
+		let buttonStyle = {
+				display:"inline-block",
+                position:"absolute",
+				width : "40", 
+				height : "40",
+				top:"-60%",
+				left:"75%",
+        };
+		
+		let divStyle = {
+				display:"inline-block",
+                position:"relative"
+        };
+
+        let containerStyle = {
+            flexGrow: 1,
+            overflow: "auto",
+            padding: 5,
+            display: "flex"
+        };
+
+        let style = this.getStyle();
+
+        if(type == "horizontal") {
+            style.flexDirection = undefined
+        }
+
+
+        return (
+            <Container style={style}>
+                <h1 style={titleStyle}>{this.props.title}</h1>
+					<div style = {divStyle}>
+					<hr style={hrStyle}>
+					</hr>
+					<img src={button} style={buttonStyle} />
+					</div>
+                <div style={containerStyle}>
+                    {this.props.children}
+                </div>
+
+            </Container>
+        );
+    }
+}
+
+Section.propTypes = {};
+Section.defaultProps = {};
+
+export default Section;
